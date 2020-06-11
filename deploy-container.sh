a180b100f1e3101f54aeca5be40ea8b3944b1776
claudius <claudius.teodorescu@gmail.com>
2020-06-11 11:54:13 +0300

    favicon

--- deploy-container.sh
+++ deploy-container.sh
@@ -8,18 +8,18 @@ data_git_url=ssh://claudius@188.212.37.221/home/git/solirom/citation-corpus-data
 output_dir=./tmp/html/
 
 
-go build -tags netgo -a -v
+#go build -tags netgo -a -v
 
 # transform the data in HTML format
 mkdir -p ./tmp/${project_name}
 
-git clone -b master --depth=1 $data_git_url ./tmp/${project_name}
-rmdir -rf ./tmp/${project_name}/.git
+#git clone -b master --depth=1 $data_git_url ./tmp/${project_name}
+rm -rf ./tmp/${project_name}/.git
 
 rm -rf "$output_dir"
 mkdir "$output_dir"
 
-java -jar ~/workspace/repositories/git/solirom-xquery-service/bin/saxon9he.jar -s:"./tmp/${project_name}/files/xml/" -xsl:"./static/modules/tei-to-html/tei-to-html.xsl" -o:"$output_dir"
+java -jar ~/workspace/repositories/git/solirom-xquery-service/bin/saxon9he.jar -s:"./tmp/${project_name}/files/xml/" -xsl:"./static/modules/tei-to-html/tei-to-html.xsl" -o:"$output_dir" 2>error.log
 
 #time sudo docker build --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" -t ${project_name} .
 
---
+++ static/modules/generate-index/create-index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+echo $current_dir
+xml_files_dir=/var/web/solirom-site/modules/citations/citation-corpus-data/files/xml
+index_name="citation-corpus"
+json_files_dir="$current_dir"/json
+
+rm -rf "$json_files_dir"
+mkdir "$json_files_dir"
+time java -jar /var/web/solirom-xquery-service/saxon9he.jar -s:"$xml_files_dir" -xsl:"generate-index.xsl" -o:"$json_files_dir/"
+
+curl -X DELETE http://localhost:8095/api/$index_name
+curl -X PUT http://localhost:8095/api/$index_name -d @"index.json"
+
+cd "$current_dir"
+time for file_name in  "$json_files_dir"/*.json
+do
+    base_name=$(basename "$file_name" | cut -d. -f1)
+    curl -f -X PUT http://localhost:8095/api/$index_name/$base_name -d @$file_name
+done
